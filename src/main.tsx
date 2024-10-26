a6b0ea50ea4abc5bf8735f48831fa94e3dbed999
Andrew Kozinski <andrewkozinski76@gmail.com>
2024-10-25 21:37:39 -0400

    removed unused navbar import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar';
 import Resume from './routes/Resume.tsx';
 
 createRoot(document.getElementById('root')!).render(

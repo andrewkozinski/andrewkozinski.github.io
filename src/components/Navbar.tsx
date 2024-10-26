de7409e112ac91a199ebdc2fcfb503d99db4d44c
Andrew Kozinski <andrewkozinski76@gmail.com>
2024-10-26 19:12:47 -0400

    Added icons to the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,10 @@
 import {Link} from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
+import homeImg from '../resources/images/homeicon.png';
+import aboutMeImg from '../resources/images/searchanddestroy.png';
+import projectsIcon from '../resources/images/projects.png';
+import contactMeIcon from'../resources/images/contactmelarge.png';
+import resumeIcon from '../resources/images/deskicon.png';
 
 
 const Navbar = () => {
@@ -7,11 +12,11 @@ const Navbar = () => {
         <header>
             <div className="nav-links">
                 <ul className="nav-ul">
-                    <li className="menu-item"><Link to="/">HOME</Link></li>
-                    <li className="menu-item"><HashLink to="/#aboutMe">ABOUT ME</HashLink></li>
-                    <li className="menu-item"><a>PROJECTS</a></li>
-                    <li className="menu-item"><a>CONTACT ME</a></li>
-                    <li className="menu-item"><Link to="/resume">RESUME</Link></li>
+                    <li className="menu-item"><Link to="/">HOME <img height={"25px"} width={"25px"} src={homeImg} style={{paddingLeft: "5px"}}/></Link></li>
+                    <li className="menu-item"><HashLink to="/#aboutMe">ABOUT ME <img height={"25px"} width={"25px"} src={aboutMeImg} style={{paddingLeft: "5px"}}/></HashLink></li>
+                    <li className="menu-item"><a>PROJECTS <img height={"25px"} width={"25px"} src={projectsIcon} style={{paddingLeft: "5px"}}/></a></li>
+                    <li className="menu-item"><a>CONTACT ME <img height={"25px"} width={"25px"} src={contactMeIcon} style={{paddingLeft: "5px"}}/></a></li>
+                    <li className="menu-item"><Link to="/resume">RESUME <img height={"25px"} width={"25px"} src={resumeIcon} style={{paddingLeft: "5px"}}/></Link></li>
                 </ul>
             </div>
         </header>

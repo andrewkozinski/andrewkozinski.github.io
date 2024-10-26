9167a3a842613cc793fc71600de3a9095649d096
Andrew Kozinski <andrewkozinski76@gmail.com>
2024-10-25 21:16:09 -0400

    Added a simple not found message

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/resume" element={<Resume />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,

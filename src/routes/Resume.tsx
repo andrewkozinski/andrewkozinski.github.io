11a215e7d3591f26b8e91ab19e2ed0d6352f3e7e
Andrew Kozinski <andrewkozinski76@gmail.com>
2024-10-26 17:26:34 -0400

    PDF page now contains pdf inside an info tab

--- src/routes/Resume.tsx
+++ src/routes/Resume.tsx
@@ -7,14 +7,29 @@ const Resume = () => {
     return (
         <>
             <Navbar />
-            <div className="main-body" id="main-body" style={{ height: "100vh", overflow: "hidden" }}>
-                <iframe
-                    src={ResumePdf}
-                    width="100%"
-                    height="100%"
-                    title="Resume"
-                    style={{ border: "none", marginTop: "20px", height: "calc(100vh - 20px)" }}
-                />
+
+            <div className="main-body" id="main-body">
+
+                <div className="info-tab" style={{paddingTop: "10px"}}>
+
+                    <div className="upper-tab" id="aboutMe">
+                        <p>Resume:</p>
+                    </div>
+
+                    <div className="lower-tab">
+                        <div className="inner-tab background align-items-center">
+                            <iframe
+                                src={ResumePdf}
+                                width="100%"
+                                height="100%"
+                                title="Resume"
+                                style={{ border: "none", marginTop: "20px", height: "calc(100vh - 20px)" }}
+                            />
+                        </div>
+                    </div>
+                </div> {/*End info tab*/}
+
+                
             </div>
         </>
         

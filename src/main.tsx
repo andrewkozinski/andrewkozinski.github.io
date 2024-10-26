35c7f28b840c79c5afc71a5bf45bfb73d8b33d93
Andrew Kozinski <andrewkozinski76@gmail.com>
2024-10-25 22:38:28 -0400

    More changes to hopefully account for react routing and 404 errors

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,29 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Resume from './routes/Resume.tsx';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+const RedirectHandler = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirectTo = params.get('redirect');
+    if (redirectTo) {
+      navigate(redirectTo);
+    }
+  }, [navigate]);
+
+  return null;
+};
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <RedirectHandler />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/resume" element={<Resume />} />

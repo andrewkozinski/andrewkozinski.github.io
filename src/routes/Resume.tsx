a34a820681fa866266f5f8c29bb71a6bf078df9b
Andrew Kozinski <andrewkozinski76@gmail.com>
2024-12-30 14:38:47 -0500

    Resume page now uses the InfoTab component

--- src/routes/Resume.tsx
+++ src/routes/Resume.tsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import ResumePdf from "../resources/pdfs/Resume - Andrew Kozinski.pdf"
 import { setTitle, setFavicon } from "../funcs";
 import ResumePng from "../resources/images/deskicon.png";
+import InfoTab from "../components/InfoTab";
 
 const Resume = () => {
 
@@ -14,27 +15,15 @@ const Resume = () => {
             <Navbar />
 
             <div className="main-body" id="main-body">
-
-                <div className="info-tab" style={{paddingTop: "35px"}}>
-
-                    <div className="upper-tab" id="aboutMe">
-                        <p>Resume:</p>
-                    </div>
-
-                    <div className="lower-tab">
-                        <div className="inner-tab background align-items-center">
-                            <iframe
-                                src={ResumePdf}
-                                width="100%"
-                                height="100%"
-                                title="Resume"
-                                style={{ border: "none", height: "calc(100vh - 20px)" }}
-                            />
-                        </div>
-                    </div>
-                </div> {/*End info tab*/}
-
-                
+                <InfoTab text="Resume:" content={
+                    <iframe
+                    src={ResumePdf}
+                    width="100%"
+                    height="100%"
+                    title="Resume"
+                    style={{ border: "none", height: "calc(100vh - 20px)" }}
+                    />
+                }/>
             </div>
         </>
         

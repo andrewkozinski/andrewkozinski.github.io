762b27189e29ee0a0f6225a84648f6834c5fdf4d
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-01-06 20:38:24 -0500

    Added contact me form to contact me page

--- src/routes/ContactMe.tsx
+++ src/routes/ContactMe.tsx
@@ -8,7 +8,16 @@ const ContactMe = () => {
             <div className="main-body" id="main-body">
                 <InfoTab text="Contact Me:" content={
                     <>
-                        <h2>Feel free to contact me through of any of the means below:</h2>
+                        
+                        <form action="https://formspree.io/f/mleovjzq" method="POST">
+                            <label htmlFor="name">Name:</label>
+                            <input type="text" id="name" name="name" placeholder="Your name" required/>
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" id="email" name="email" placeholder="Your email" required/>
+                            <label htmlFor="message">Message:</label>
+                            <textarea id="message" name="message" placeholder="Write a message" required></textarea>
+                            <input type="submit" value="Submit"/>
+                        </form>
                     </>
                 }/>
             </div>

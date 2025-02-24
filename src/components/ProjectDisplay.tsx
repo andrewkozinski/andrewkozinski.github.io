8d4e442376ae0be80f65ca4218471bc6f3973256
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-02-24 18:17:45 -0500

    Made font size bigger, actually used the technologies used json field

--- src/components/ProjectDisplay.tsx
+++ src/components/ProjectDisplay.tsx
@@ -7,12 +7,12 @@ const ProjectDisplay = (project: Project) => {
 
             <div className="project-inner-upper">
                 <a href={project.projLink}><img src={project.previewImg} height="69px" width="184px" /></a>
-                <a href={project.projLink} style={{paddingLeft: '15px', display: "block", marginBlockStart: "1em", marginBlockEnd: "1em", marginInlineStart: "0px", marginInlineEnd: "0px"}}>{project.title}</a>
+                <a href={project.projLink} style={{paddingLeft: '15px', display: "block", marginBlockStart: ".5em", marginBlockEnd: "1em", marginInlineStart: "0px", marginInlineEnd: "0px", fontSize: "15px"}}>{project.title}</a>
             </div>
 
             <div className="project-inner-lower align-items-center">
-                <p style={{ color: 'white' }}>Languages Used</p>
-                <p style={{ paddingLeft: '8px', paddingRight: '8px' }}>5 of 118</p>
+                <p style={{ color: 'white' }}>Technologies Used: {project.technologiesUsed}</p>
+                {/*<p style={{ paddingLeft: '8px', paddingRight: '8px' }}>{project.technologiesUsed}</p> */}
                 <div className="proj-languages"></div>
             </div>
 

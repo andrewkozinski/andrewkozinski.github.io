38946e100db601ef79290a7c63652e6361163289
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-02-25 21:23:45 -0500

    Added open class to the nav-ul className (class only applied when hamburger menu is clicked)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,11 +15,11 @@ const Navbar = () => {
     return (
         <header>
             <div className="nav-links">
-                <ul className="nav-ul">
+                <ul className={`nav-ul ${isOpen ? 'open' : ''}`}>
                     <li className="menu-item"><Link to="/">HOME</Link></li>
                     <li className="menu-item"><HashLink to="/#aboutMe">ABOUT ME</HashLink></li>
-                    <li className="menu-item"><Link to ="/projects">PROJECTS</Link></li>
-                    <li className="menu-item"><Link to ="/contactme">CONTACT ME</Link></li>
+                    <li className="menu-item"><Link to="/projects">PROJECTS</Link></li>
+                    <li className="menu-item"><Link to="/contactme">CONTACT ME</Link></li>
                     <li className="menu-item"><Link to="/resume">RESUME</Link></li>
                 </ul>
             </div>

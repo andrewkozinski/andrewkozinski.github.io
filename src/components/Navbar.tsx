976555c4f218b060ae67d25f796c71dc6e81d1d6
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-03-04 23:49:31 -0500

    Navbar altered to handle mobile hamburger menu functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,17 @@ import {Link} from 'react-router-dom';
 import { useState } from 'react';
 import { HashLink } from 'react-router-hash-link';
 
+const NavElements = () => {
+    return (
+        <>
+            <li className="menu-item"><Link to="/">HOME</Link></li>
+            <li className="menu-item"><HashLink to="/#aboutMe">ABOUT ME</HashLink></li>
+            <li className="menu-item"><Link to="/projects">PROJECTS</Link></li>
+            <li className="menu-item"><Link to="/contactme">CONTACT ME</Link></li>
+            <li className="menu-item"><Link to="/resume">RESUME</Link></li>
+        </>
+    );
+}
 
 const Navbar = () => {
     
@@ -13,20 +24,27 @@ const Navbar = () => {
 
     
     return (
+        <>
         <header>
             <div className="nav-links">
-                <ul className={`nav-ul ${isOpen ? 'open' : ''}`}>
-                    <li className="menu-item"><Link to="/">HOME</Link></li>
-                    <li className="menu-item"><HashLink to="/#aboutMe">ABOUT ME</HashLink></li>
-                    <li className="menu-item"><Link to="/projects">PROJECTS</Link></li>
-                    <li className="menu-item"><Link to="/contactme">CONTACT ME</Link></li>
-                    <li className="menu-item"><Link to="/resume">RESUME</Link></li>
+                {/*Desktop Navbar*/}
+                <ul className={`nav-ul`}>
+                    <NavElements />
                 </ul>
             </div>
             <div className="hamburger-menu" onClick={toggleMenu}>
                 &#9776;
             </div>
         </header>
+        
+        {/*Mobile Hamburger Menu*/}
+        <div className={`mobile-menu ${isOpen ? 'open' : ''}`}>
+                <ul className="">
+                    <NavElements />
+                </ul>
+        </div>
+        
+        </>
     );
 }
 

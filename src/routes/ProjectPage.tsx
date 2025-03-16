1f2718cfa5b5077a31be293f08c13ecf1f384d5e
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-03-16 11:27:54 -0400

    console log call on params to not have the github page crash

--- src/routes/ProjectPage.tsx
+++ src/routes/ProjectPage.tsx
@@ -4,12 +4,13 @@ import {useParams} from "react-router-dom";
 const ProjectPage = () => {
 
   let params = useParams();
-  
+  console.log(params);
+
   return (
     <>
         <Navbar/>
         <div className="main-body" id="main-body">
-            
+            <p>{params.data}</p>
         </div>
     </>
   );

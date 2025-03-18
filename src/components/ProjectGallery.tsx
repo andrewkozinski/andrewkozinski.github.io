ababff8cc914d69f0e49c7226fab75e53464304f
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-03-18 13:24:19 -0400

    Removed unused import

--- src/components/ProjectGallery.tsx
+++ src/components/ProjectGallery.tsx
@@ -1,5 +1,5 @@
 import { ChevronLeft, ChevronRight } from 'lucide-react';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './ProjectGallery.css';
 
 interface ProjectGalleryProps {

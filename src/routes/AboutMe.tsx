70a43ed2ed00bf2dcb7e18cd0d9ce34d8a1868f1
Andrew Kozinski <andrewkozinski76@gmail.com>
2025-03-28 19:07:23 -0400

    Created AboutMe page tsx file

---
+++ src/routes/AboutMe.tsx
@@ -0,0 +1,11 @@
+const AboutMe = () => {
+    return (
+        <div className="about-me">
+        <h1>About Me</h1>
+        <p>
+            "Hello! This is a test page that shouldn't be accessible yet! Stay tuned!"
+        </p>
+        </div>
+    );
+}
+export default AboutMe;
